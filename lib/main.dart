009efacc549d6de6e234b1921933086d96c8ad84
ivan-lab-pvls <ivan.labasok@pvls.tech>
2024-04-03 19:01:57 +0200

    dfs

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:lucky_8/csdcawea/luc_splash_uuu.dart';
 
-const lucPrivacyUuu = 'https://www.google.com';
-const lucTermsUuu = 'https://www.google.com';
+const lucPrivacyUuu =
+    'https://docs.google.com/document/d/1SkfDTEnUM9F9sb0dbZfj63FY9j18QNv6v2TBYiDExv4/edit?usp=sharing';
+const lucTermsUuu =
+    'https://docs.google.com/document/d/1JwDod8rpVNEoFlz1rCjSEHh-dcOehhthUzFbvfCiSbo/edit?usp=sharing';
 
 void main() {
   runApp(
